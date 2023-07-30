570850a7cd0e3a00bb5bc4b3dbb436d2ca618c89
deividcuello <deividcuello@gmail.com>
2023-07-30 18:20:02 -0400

    first commit

---
+++ dist/js/script.js
@@ -0,0 +1,30 @@
+const fullName = document.querySelector('.name'),
+avatar = document.querySelector('.avatar'),
+username = document.querySelector('.username'),
+bio = document.querySelector('.bio'),
+followers = document.querySelector('.followers'),
+repositories = document.querySelector('.repositories'),
+url = document.querySelector('.url')
+input = document.querySelector('input[type=text]')
+submit = document.querySelector('input[type=submit]');
+console.log(submit)
+
+fetchUser('torvalds')
+
+async function fetchUser(search){
+    const user = await fetch(`https://api.github.com/users/${search}`);
+    const res = await user.json();
+    console.log(res)
+    avatar.src = res.avatar_url;
+    fullName.innerHTML = res.name || 'No available';
+    username.innerHTML = `@${res.login}`;
+    bio.innerHTML = res.bio || 'No available';
+    followers.innerHTML = res.followers + ' followers' || 'No available';
+    repositories.innerHTML = res.public_repos + ' repositories' || 'No available';;
+    url.innerHTML = res.blog;
+    url.href = res.blog;
+}
+
+submit.addEventListener('click', () => {
+    fetchUser(input.value)
+})
